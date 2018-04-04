dd667d749e1caf0ee8b037d7326f56014c5a04a7
denimdatta <d.dat08@gmail.com>
2018-04-03 19:49:57 -0700

    KMeans modified

--- KMeans.py
+++ KMeans.py
@@ -1,14 +1,13 @@
 # Name: Denim Datta
 
 import time
-
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def read_data(filename):
     dataset = []
-
     with open(filename, "r") as file:
         data = file.readlines()
 
@@ -26,9 +25,7 @@ def kmean_cluster(data, k):
     centroids = [feature for feature in data[0:k]]
     centroids.sort()
 
-    tempRound = 0
     while True:
-        tempRound += 1
         region = {}
         for i in range(k):
             region[i] = []
@@ -36,35 +33,16 @@ def kmean_cluster(data, k):
         for feature in data:
             n_feature = np.array(feature)
             n_centroid = [np.array(centroid) for centroid in centroids]
-            euclidian = []
-            for centroid in n_centroid:
-                euclidian.append(np.linalg.norm(centroid - n_feature))
+            # euclidian = []
+            # for centroid in n_centroid:
+            #     euclidian.append(np.linalg.norm(centroid - n_feature))
+            euclidian = [np.linalg.norm(centroid - n_feature) for centroid in n_centroid]
+
             min_index = euclidian.index(min(euclidian))
             region[min_index].append(feature)
 
-        empty_region_count = 0
-        empty_regions = []
-        largest_region_index = 0
-        for r in region.keys():
-            if len(region[r]) > len(region[largest_region_index]):
-                largest_region_index = r
-
-            if len(region[r]) == 0:
-                empty_region_count += 1
-                empty_regions.append(r)
-
-        if empty_region_count > 0:
-            for i in range(empty_region_count):
-                region_index = empty_regions[i]
-                region[region_index].append(region[largest_region_index][i])
-            region[largest_region_index] = region[largest_region_index][empty_region_count:]
-
+        region = check_empty_region(region)
         centroids_updated = find_centroids(region)
-        # for r in region.keys():
-        #     print("K:{} -- S:{}\nV:{}\n".format(r, len(region[r]), region[r]))
-        # for c in centroids_updated:
-        #     print("New: {}".format(c))
-
         converged = True
 
         for i in range(k):
@@ -77,9 +55,28 @@ def kmean_cluster(data, k):
         else:
             centroids = centroids_updated
 
-    print("[Total] {}".format(tempRound))
-
     return centroids, region
+
+
+def check_empty_region(region):
+    empty_region_count = 0
+    empty_regions = []
+    largest_region_index = 0
+    for r in region.keys():
+        if len(region[r]) > len(region[largest_region_index]):
+            largest_region_index = r
+
+        if len(region[r]) == 0:
+            empty_region_count += 1
+            empty_regions.append(r)
+
+    if empty_region_count > 0:
+        for i in range(empty_region_count):
+            region_index = empty_regions[i]
+            region[region_index].append(region[largest_region_index][i])
+        region[largest_region_index] = region[largest_region_index][empty_region_count:]
+
+    return region
 
 
 def find_centroids(regions):
@@ -87,7 +84,6 @@ def find_centroids(regions):
     new_centroids = [[0 for i in range(feature_size)] for j in range(len(regions))]
     for r in regions:
         new_centroids[r] = (np.average(regions[r], axis=0)).tolist()
-    # new_centroids = new_centroids.tolist()
     new_centroids.sort()
     return new_centroids
 
@@ -98,10 +94,8 @@ def potential_fn(centroid, region):
     for i in range(len(centroid)):
         dist = [np.linalg.norm(np.array(centroid[i]) - data) for data in region[i]]
         for d in dist:
-            # potential += (d*d)
             potential += d
-        # print("DL:{} -- DSL:{}\nDIST: {}".format(len(region[i]), len(dist), dist))
-    print(potential)
+
     return potential
 
 
@@ -112,17 +106,14 @@ def main():
 
     for k in clusters:
         (centroids, regions) = kmean_cluster(feature_data, k)
-        # for c in centroids:
-        #     print("C: {}".format(c))
-        # for r in regions:
-        #     print("R: {}".format(regions[r]))
         potential = potential_fn(centroids, regions)
         potentials.append(potential)
+        print("K value: {0}      Potential function value: {1}".format(k, potential))
 
-    # plt.ylim(int(math.floor(min(potentials)/10.0)) *10 , int(math.ceil((max(potentials) + 1) / 10.0)) * 10)
-    plt.ylabel("Potential Function")
+    plt.ylim(int(math.floor((min(potentials) - 1) / 100.0)) * 100, int(math.ceil((max(potentials) + 1) / 100.0)) * 100)
+    plt.ylabel("Potential function value")
     plt.xlabel("K Value")
-    plt.title("Potential Function Vs. K value")
+    plt.title("Potential function value  Vs.  K value")
     plt.plot(clusters, potentials, 'rv--')
     plt.show()
 
